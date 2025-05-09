d478b560f636193ea89504540d7ccecfee85629d
Moaz Eldefrawy <moaz@bloompath.ai>
2025-05-09 21:15:06 +0300

    Refactor: remove unused datetime import from main.py to streamline code

--- server/api/main.py
+++ server/api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 from typing import List, Optional
-from datetime import date, datetime, timedelta
+from datetime import date, timedelta
 from enum import Enum
 
 app = FastAPI(
