af52245127b34fb163e4640c97b4313ec9af1b28
Moaz Eldefrawy <moaz@bloompath.ai>
2025-05-14 19:44:57 +0300

    Add: Google OAuth integration with environment variable setup; include .env.example and update .gitignore to exclude .env file.

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+if (!clientId) {
+  throw new Error('Google Client ID is not defined in environment variables');
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>
 );
